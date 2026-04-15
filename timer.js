be361f2f57301dad7c49926f6b5ab51169048ab2
Rajesh <rajesh@example.com>
2026-04-15 18:12:58 +0530

    commit

--- timer.js
+++ timer.js
@@ -43,25 +43,3 @@ export default class Timer {
 
 
 
-//Ui handling:
-
-export class UI {
-  constructor() {
-    this.timeDisplay = document.getElementById("time");
-    this.modeDisplay = document.getElementById("mode");
-  }
-
-  formatTime(seconds) {
-    const m = String(Math.floor(seconds / 60)).padStart(2, "0");
-    const s = String(seconds % 60).padStart(2, "0");
-    return `${m}:${s}`;
-  }
-
-  updateTime(seconds) {
-    this.timeDisplay.textContent = this.formatTime(seconds);
-  }
-
-  updateMode(mode) {
-    this.modeDisplay.textContent = mode;
-  }
-}
